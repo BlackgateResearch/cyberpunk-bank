d1e1719f6e8b533c2a02a480eb0c5455c32e397f
Karl Williams <karl@deadlight.net>
2014-07-28 20:35:23 +0000

    error messages

--- bank/views.py
+++ bank/views.py
@@ -9,7 +9,7 @@ def account_view(request, account_number=None):
     context = {'account': account}
     if payee and amount:
         if account.balance - int(amount) < 0:
-            context['message'] = {'text': 'not enough funds', 'type': 'error'}
+            context['message'] = {'title': 'Error', 'text': 'Not enough funds', 'type': 'error'}
             return render_to_response('account.html', context)
         account.balance = account.balance - int(amount)
         account.save()
@@ -17,6 +17,6 @@ def account_view(request, account_number=None):
         current_balance = p.balance
         p.balance = current_balance + int(amount)
         p.save()
-        context['message'] = {'text': 'updated', 'type': 'confirmation'}
+        context['message'] = {'title': 'Transfer successful', 'text': amount+' credits transferred', 'type': 'confirmation'}
     
     return render_to_response('account.html', context)
