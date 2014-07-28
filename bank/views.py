7f694a1a52fbdb5adac9fbfe73fe2badc7980df9
Tristram Oaten <tristram@oaten.name>
2014-07-28 21:19:11 +0100

    page works

--- bank/views.py
+++ bank/views.py
@@ -4,4 +4,14 @@ from bank.models import Account
 
 def account_view(request, account_number=None):
     account = get_object_or_404(Account, pk=account_number)
-    return render_to_response('account.html', {'account': account})
+    payee = request.GET.get('payee')
+    amount = request.GET.get('amount')
+    context = {'account': account}
+    if payee and amount:
+        p = Account.objects.get(pk=int(payee))
+        current_balance = p.balance
+        p.balance = current_balance + int(amount)
+        p.save()
+        context['message'] = {'text': 'updated', 'type': 'confirmation'}
+    
+    return render_to_response('account.html', context)
